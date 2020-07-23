504ba8b1781eac74f046f01521620ef9aed864e3
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-23 21:38:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,7 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-  array.shift(element)
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
   return array
 }
